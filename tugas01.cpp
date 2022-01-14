e1820be89bd36da00fd0f261f27f0ed37403d7c4
DITASRITANTI <96286691+DITASRITANTI@users.noreply.github.com>
2022-01-14 10:20:34 +0700

    Add files via upload

---
+++ tugas01.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+	
+char nama[25];
+int nim;
+char prodi [50];
+
+printf(	"DITA SRITANTI :");
+printf("07.1.2021.0023 :");
+printf("TEKNIK	INFORMATIKA :");
+
+
+return 0;
+}
