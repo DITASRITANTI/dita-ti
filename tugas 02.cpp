ca85cee882256d781cf09c9aca9ad0fcb7a92040
DITASRITANTI <96286691+DITASRITANTI@users.noreply.github.com>
2022-01-14 10:22:33 +0700

    Add files via upload

---
+++ tugas 02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,t;
+	float luas;
+	cout<<"masukkan panjang alas:";
+	cin>>a;
+	cout<<"masukkan tinggi segitiga:";
+	cin>> t;
+	luas=0.5*a*t;
+	cout << "luas segitiga adalah "
+	<<luas<< endl;
+	
+	return 0;
+}
